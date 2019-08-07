576a1573adf21588835ff4937fba58cbe216248e
Jack Royal <jroyal@oucs1660.otago.ac.nz>
2019-08-07 14:44:32 +1200

    Swapped out html file with php file

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>The begginings of assignment 1</title>
+</head>
+<body>
+
+<h1>Where to from now.</h1>
+<p>Time to create multiple VMs</p>
+
+</body>
+</html>
