481f861d7c0b9e084e246b3814792c5b3eda7675
Jack Royal <jroyal@oucs1615.otago.ac.nz>
2019-08-20 14:58:31 +1200

    Added comments to dbserverscript

--- dbserverscript.sh
+++ dbserverscript.sh
@@ -10,11 +10,11 @@ echo "mysql-server mysql-server/root_password_again password $MYSQL_PWD" | debco
 # Install the MySQL database server.
 apt-get -y install mysql-server
 
+#Added for easier provisioning
 echo "DROP DATABASE IF EXISTS jacksdb;" | mysql
 echo "DROP USER IF EXISTS 'jackuser1', remote;" | mysql
 
-# Run some setup commands to get the database ready to use.
-# First create a database.
+# Create database that will contain tables
 echo "CREATE DATABASE jacksdb;" | mysql
 
 # Then create a database user "jackuser1" with the given password.
@@ -22,17 +22,14 @@ echo "CREATE USER 'jackuser1'@'%' IDENTIFIED BY 'password1';" | mysql
 
 echo "GRANT ALL PRIVILEGES ON jacksdb.* TO 'jackuser1'@'%'" | mysql
 
-# Set the MYSQL_PWD shell variable that the mysql command will
-# try to use as the database password ...
+# Export password
 export MYSQL_PWD='password1'
 
-#The mysql command specifies both
-# the user to connect as (webuser) and the database to use (fvision).
+#add user jackuser1 and jacksdb
 cat /vagrant/testdb.sql | mysql -u jackuser1 jacksdb
 
-#Allow public connection
+# The /0.0.0.0/ allows for public connections to occur
 sed -i'' -e '/bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# We then restart the MySQL server to ensure that it picks up
-# our configuration changes.
+# Restart for configuration comments
 service mysql restart
